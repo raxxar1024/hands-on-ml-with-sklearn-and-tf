6a58c18f8b24e57c1b77171102e2452ce9b8bdab
yjw013809 <yjw013809@gmail.com>
2018-08-24 23:15:51 +0800

    02_end_to_end_machine_learning_project

--- 02/02_end_to_end_machine_learning_project.py
+++ 02/02_end_to_end_machine_learning_project.py
@@ -39,26 +39,35 @@ if __name__ == "__main__":
     # housing.hist(bins=50, figsize=(20, 15))
     # plt.show()
 
-    train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
-    housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
-    housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+    # train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+    # housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+    # housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+    #
+    # split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    # for train_index, test_index in split.split(housing, housing["income_cat"]):
+    #     strat_train_set = housing.loc[train_index]
+    #     strat_test_set = housing.loc[test_index]
+    #     print(housing["income_cat"].value_counts() / len(housing))
+    #
+    #     for set in (strat_train_set, strat_test_set):
+    #         set.drop(["income_cat"], axis=1, inplace=True)
+    #
+    #     housing = strat_train_set.copy()
+    #     # housing.plot(kind="scatter", x="longitude", y="latitude")
+    #     # housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+    #     # plt.show()
+    #
+    #     housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+    #                  s=housing["population"] / 100, label="population",
+    #                  c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
+    #     plt.legend()
+    #     plt.show()
 
-    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-    for train_index, test_index in split.split(housing, housing["income_cat"]):
-        strat_train_set = housing.loc[train_index]
-        strat_test_set = housing.loc[test_index]
-        print(housing["income_cat"].value_counts() / len(housing))
+    from pandas.tools.plotting import scatter_matrix
 
-        for set in (strat_train_set, strat_test_set):
-            set.drop(["income_cat"], axis=1, inplace=True)
-
-        housing = strat_train_set.copy()
-        # housing.plot(kind="scatter", x="longitude", y="latitude")
-        # housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
-        # plt.show()
-
-        housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
-                     s=housing["population"] / 100, label="population",
-                     c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
-        plt.legend()
-        plt.show()
+    # attributes = ["median_house_value", "median_income", "total_rooms",
+    #               "housing_median_age"]
+    # scatter_matrix(housing[attributes], figsize=(12, 8))
+    housing.plot(kind="scatter", x="median_income", y="median_house_value",
+                 alpha=0.1)
+    plt.show()
